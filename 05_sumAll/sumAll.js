c35f97e5e913914edd99865c2bacb0ce181d6f3a
sadibartin <sadibartin@gmail.com>
2023-03-30 02:09:42 +0300

    ez

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
-};
+const sumAll = function(min,max) {
+    if (!number.isInteger(min) || !number.isInteger(max)) return "ERROR";
+    
+    
+    if(min<0 || max<0) return "ERROR" ; 
+    if(min>max){
+        const esitle;
+        min=max;
+        max=esitle;
+        
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
